d7f8c8b5d11ba42f27bf7dffabac7dce5a8c08d2
Nicola Verbeeck <nicola@chimerapps.com>
2016-05-23 09:04:34 +0200

    Updated crittercism logger to v2.0.4

--- CrittercismCrashReporter/lib/src/main/java/crashreporter/crittercism/CrittercismCrashReporter.java
+++ CrittercismCrashReporter/lib/src/main/java/crashreporter/crittercism/CrittercismCrashReporter.java
@@ -2,10 +2,13 @@ package crashreporter.crittercism;
 
 import android.content.Context;
 import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
 
 import com.crittercism.app.Crittercism;
 import com.crittercism.app.CrittercismConfig;
 import com.icapps.crashreporter.CrashReporter;
+
+import java.net.URL;
 
 /**
  * @author Nicola Verbeeck
@@ -21,12 +24,18 @@ public class CrittercismCrashReporter implements CrashReporter {
 	}
 
 	@Override
-	public void setUserIdentifier(final String userIdentifier) {
+	public void setUserIdentifier(@Nullable final String userIdentifier) {
+		if (userIdentifier == null) {
+			return;
+		}
 		Crittercism.setUsername(userIdentifier);
 	}
 
 	@Override
-	public void leaveBreadcrumb(final String breadCrumb) {
+	public void leaveBreadcrumb(@Nullable final String breadCrumb) {
+		if (breadCrumb == null) {
+			return;
+		}
 		Crittercism.leaveBreadcrumb(breadCrumb);
 	}
 
@@ -36,18 +45,29 @@ public class CrittercismCrashReporter implements CrashReporter {
 	}
 
 	@Override
-	public void logHandledException(final Throwable handledError) {
+	public void logHandledException(@Nullable final Throwable handledError) {
+		if (handledError == null) {
+			return;
+		}
 		Crittercism.logHandledException(handledError);
 	}
 
 	@Override
-	public void logData(final String key, final Object value) {
+	public void logData(final String key, @Nullable final Object value) {
 		//Not supported
 	}
 
 	@Override
 	public boolean didCrashLastSession() {
 		return Crittercism.didCrashOnLastLoad();
+	}
+
+	@Override
+	public void logNetworkRequest(@Nullable final String method, @Nullable final String url, final int responseCode) {
+		try {
+			Crittercism.logNetworkRequest(method == null ? "Unknown" : method, new URL(url == null ? "http://unknown" : url), 0, 0, 0, responseCode, null);
+		} catch (final Throwable ignore) {
+		}
 	}
 
 	public static class Config {
