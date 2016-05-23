bf98fe79354b6fd8716c020d57c561ab6925a74f
Nicola Verbeeck <nicola@chimerapps.com>
2016-05-23 08:56:55 +0200

    Updated to version 2.0.4 of crashlytics

--- GoogleanAlyticsCrashReporter/lib/src/main/java/crashreporter/googleanalytics/GoogleAnalyticsCrashReporter.java
+++ GoogleanAlyticsCrashReporter/lib/src/main/java/crashreporter/googleanalytics/GoogleAnalyticsCrashReporter.java
@@ -2,6 +2,7 @@ package crashreporter.googleanalytics;
 
 import android.content.Context;
 import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
 
 import com.google.android.gms.analytics.GoogleAnalytics;
 import com.google.android.gms.analytics.HitBuilders;
@@ -33,27 +34,54 @@ public class GoogleAnalyticsCrashReporter implements CrashReporter {
 
 	@Override
 	public void leaveBreadcrumb(final String breadCrumb) {
+		if (breadCrumb == null) {
+			return;
+		}
 		mTracker.send(new HitBuilders.EventBuilder().setCategory("breadcrumb").setAction(breadCrumb).setLabel("").build());
 	}
 
 	@Override
-	public void logEvent(final String event) {
+	public void logEvent(@Nullable final String event) {
+		if (event == null) {
+			return;
+		}
 		mTracker.send(new HitBuilders.EventBuilder().setCategory("event").setAction(event).setLabel("").build());
 	}
 
 	@Override
-	public void logHandledException(final Throwable handledError) {
+	public void logHandledException(@Nullable final Throwable handledError) {
+		if (handledError == null) {
+			return;
+		}
 		mTracker.send(new HitBuilders.ExceptionBuilder().setDescription(constructMessage(handledError)).setFatal(false).build());
 	}
 
 	@Override
-	public void logData(final String key, final Object value) {
-		mTracker.send(new HitBuilders.EventBuilder().setCategory(key).setAction(value.toString()).setLabel("").build());
+	public void logData(final String key, @Nullable final Object value) {
+		final HitBuilders.EventBuilder builder = new HitBuilders.EventBuilder().setCategory(key);
+		if (value != null) {
+			builder.setAction(value.toString());
+		}
+		builder.setLabel("");
+		mTracker.send(builder.build());
 	}
 
 	@Override
 	public boolean didCrashLastSession() {
 		return false;
+	}
+
+	@Override
+	public void logNetworkRequest(@Nullable final String method, @Nullable final String url, final int responseCode) {
+		if (method == null && url == null) {
+			return;
+		}
+
+		final HitBuilders.EventBuilder builder = new HitBuilders.EventBuilder().setCategory("network");
+		builder.setAction(method == null ? "Unknown" : method);
+		builder.setLabel(url == null ? "Unknown" : url);
+
+		mTracker.send(builder.build());
 	}
 
 	private String constructMessage(final Throwable error) {
